eaaae556be9dcc36ccca273183e09c9905f164bb
Naman Garg <naman.garg.naman@gmail.com>
2026-04-10 16:55:36 +0530

    Bump version to 0.1.11

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env bun
 
-export const VERSION = "0.1.10";
+export const VERSION = "0.1.11";
 
 const SKIP_UPDATE_CHECK = new Set(["auth", "update", "version", "--version", "-v", "help", "--help", "-h"]);
 
